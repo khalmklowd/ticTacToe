4176824a4f0597716d6ea885ce168f6f7ff77ee9
khalmklowd <kylewmcleod@hotmail.com>
2021-03-15 12:16:01 -0400

    generic bash commit

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //A work in progress.
 const game = (function() {
   let i = 2;     
-  
+  let winner = '';  
+  let winnerO = '';
   // Create gameboard with 3x3 grid
     const gameBoard = () => {
         let board = document.createElement('DIV');
@@ -14,34 +15,139 @@ const game = (function() {
     
     //function to listen for clicks and switch turns.
     const turn = function(a) {
-      document.getElementById(`box${a}`).addEventListener('click', () => {
-          if (i % 2 == 0) {
-            document.getElementById(`box${a}`).style.backgroundImage = "url(images/xForTic.svg)";
-          }
-          else {
-            document.getElementById(`box${a}`).style.backgroundImage = "url(images/blueO.jpg)";
-          }
-          i += 1;
+      
+      
+      document.getElementById(`box${a}`).addEventListener('mousedown', () => {
+        
+        if (i % 2 == 0) {
+          document.getElementById(`box${a}`).style.backgroundImage = "url(images/xForTic.svg)";
+          
+          const xWins = (function() {
+            winner += `${a}, `;
+            console.log(winner);
+            if (winner.includes('0') && winner.includes('1') && winner.includes('2')) {
+              console.log("X Wins!")
+              document.getElementById("head").innerHTML = "<h1><b>X WINS!</b></h1>";
+              setTimeout(function () { alert("New Game?"); location.reload(); }, 3000)
+            }
+            if (winner.includes('0') && winner.includes('3') && winner.includes('6')) {
+              console.log('X Wins!');
+              console.log("X Wins!")
+              document.getElementById("head").innerHTML = "<h1><b>X WINS!</b></h1>";
+              setTimeout(function () { alert("New Game?"); location.reload(); }, 3000)
+            }  
+            if (winner.includes('0') && winner.includes('4') && winner.includes('8')) {
+              console.log('X Wins!');
+              console.log("X Wins!")
+              document.getElementById("head").innerHTML = "<h1><b>X WINS!</b></h1>";
+              setTimeout(function () { alert("New Game?"); location.reload(); }, 3000)
+            }
+            if (winner.includes('3') && winner.includes('4') && winner.includes('5')) {
+              console.log('X Wins!');
+              console.log("X Wins!")
+              document.getElementById("head").innerHTML = "<h1><b>X WINS!</b></h1>";
+              setTimeout(function () { alert("New Game?"); location.reload(); }, 3000)
+            }
+            if (winner.includes('6') && winner.includes('7') && winner.includes('8')) {
+              console.log('X Wins!');
+              console.log("X Wins!")
+              document.getElementById("head").innerHTML = "<h1><b>X WINS!</b></h1>";
+              setTimeout(function () { alert("New Game?"); location.reload(); }, 3000)
+            }
+            if (winner.includes('1') && winner.includes('4') && winner.includes('7')) {
+              console.log('X Wins!');
+              console.log("X Wins!")
+              document.getElementById("head").innerHTML = "<h1><b>X WINS!</b></h1>";
+              setTimeout(function () { alert("New Game?"); location.reload(); }, 3000)
+            }
+            if (winner.includes('2') && winner.includes('5') && winner.includes('8')) {
+              console.log('X Wins!');
+              console.log("X Wins!")
+              document.getElementById("head").innerHTML = "<h1><b>X WINS!</b></h1>";
+              setTimeout(function () { alert("New Game?"); location.reload(); }, 3000)
+            }
+            if (winner.includes('2') && winner.includes('4') && winner.includes('6')) {
+              console.log('X Wins!');
+              console.log("X Wins!")
+              document.getElementById("head").innerHTML = "<h1><b>X WINS!</b></h1>";
+              setTimeout(function () { alert("New Game?"); location.reload(); }, 3000)
+            }
+          }());
+        }
+        else {
+          document.getElementById(`box${a}`).style.backgroundImage = "url(images/blueO.jpg)";
+        
+          const yWins = (function() {
+            winnerO += `${a}, `;
+            console.log(winnerO);
+            if (winnerO.includes('0') && winnerO.includes('1') && winnerO.includes('2')) {
+              console.log('O Wins!');
+              window.location.href = "https://www.youtube.com/watch?v=U9DyHthJ6LA";
+              document.getElementById("head").innerHTML = "<h1><b>O WINS!</b></h1>";
+              setTimeout(function () { alert("New Game?"); location.reload(); }, 3000)
+            }
+            if (winnerO.includes('0') && winnerO.includes('3') && winnerO.includes('6')) {
+              console.log('O Wins!');
+              document.getElementById("head").innerHTML = "<h1><b>O WINS!</b></h1>";
+              setTimeout(function () { alert("New Game?"); location.reload(); }, 3000)
+            }  
+            if (winnerO.includes('0') && winnerO.includes('4') && winnerO.includes('8')) {
+              console.log('O Wins!');
+              document.getElementById("head").innerHTML = "<h1><b>O WINS!</b></h1>";
+              setTimeout(function () { alert("New Game?"); location.reload(); }, 3000)
+            }
+            if (winnerO.includes('3') && winnerO.includes('4') && winnerO.includes('5')) {
+              console.log('O Wins!');
+              document.getElementById("head").innerHTML = "<h1><b>O WINS!</b></h1>";
+              setTimeout(function () { alert("New Game?"); location.reload(); }, 3000)
+            }
+            if (winnerO.includes('6') && winnerO.includes('7') && winnerO.includes('8')) {
+              console.log('O Wins!');
+              document.getElementById("head").innerHTML = "<h1><b>O WINS!</b></h1>";
+              setTimeout(function () { alert("New Game?"); location.reload(); }, 3000)
+            }
+            if (winnerO.includes('1') && winnerO.includes('4') && winnerO.includes('7')) {
+              console.log('O Wins!');
+              document.getElementById("head").innerHTML = "<h1><b>O WINS!</b></h1>";
+              setTimeout(function () { alert("New Game?"); location.reload(); }, 3000)
+            }
+            if (winnerO.includes('2') && winnerO.includes('5') && winnerO.includes('8')) {
+              console.log('O Wins!');
+              document.getElementById("head").innerHTML = "<h1><b>O WINS!</b></h1>";
+              setTimeout(function () { alert("New Game?"); location.reload(); }, 3000)
+            }
+            if (winnerO.includes('2') && winnerO.includes('4') && winnerO.includes('6')) {
+              console.log('O Wins!');
+              document.getElementById("head").innerHTML = "<h1><b>O WINS!</b></h1>";
+              setTimeout(function () { alert("New Game?"); location.reload(); }, 3000)
+            }
+          }());
+        }
+          i++;
         })
-}
+      
+      }
+
+      
+
     return {turn, gameBoard}
 })();
 
 game.gameBoard();
 
+
+
 // Add turn function to each div in the game board.
 const gamePlay = (function() {
-    i = 0;
+    let i = 0;
+    let box = document.getElementById(`box${i}`).style.backgrounImage = "url(images/xForTic.svg)";
+    let box1 = document.getElementById(`box${i}`).style.backgrounImage = "url(images/blueO.jpg)";
     for (i = 0; i <= 8; i++){
-      document.getElementById(`box${i}`).onclick = game.turn(i);
-    }
+      document.getElementById(`box${i}`).mousedown = game.turn(i);
+    
+  }
   }())
-const stoppit = (function() {
-    i = 0;
-    for (i = 0; i <= 8; i++){
-      document.getElementById(`box${i}`).removeEventListener("click", gamePlay);
-    }
-}())
+
 
   // something for me to reference.
 const documentMock = (() => ({
